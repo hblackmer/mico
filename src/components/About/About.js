f3b5796468c4e760da8d0fddcde87986b870ce20
Helene <hcblackmer@gmail.com>
2021-10-18 23:11:17 -0700

    Updated with About component with Reactstrap.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Card, CardBody } from 'reactstrap';
+import { Container, Row, Col, Card, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import './About.css';
 
 class About extends Component {
@@ -9,30 +9,30 @@ class About extends Component {
                 <Container className="about-container px-lg-5">
                     <h2 className="text-white text-center" id="about">What is Mico?</h2>
                     <Row>
-                        <Col md="4" className="mb-5 about-cards">
-                            <Card className="about-bg h-100">
-                                <CardBody className="text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                    <div className="about_icons"><img src="/assets/flash.png" class="about_img" /></div>
-                                    <h2 className="fs-2 fw-bold">A study tool</h2>
-                                    <p className="mb-0">Customizable and randomized questions.</p>
+                        <Col md="4" className="about-cards">
+                            <Card className="about-bg">
+                                <CardBody className="about-body p-4 p-lg-5 pt-0 pt-lg-0">
+                                    <div className="about-icons"><img src="/assets/flash.png" className="about_img" /></div>
+                                    <CardTitle tag="h2">A study tool</CardTitle>
+                                    <CardSubtitle tag="h6">Customizable and randomized questions.</CardSubtitle>
                                 </CardBody>
                             </Card>
                         </Col>
-                        <Col md="4"  className="mb-5 about-cards">
-                            <Card className="about-bg h-100">
-                                <CardBody className="text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                    <div className="about_icons"><img src="/assets/test.png" class="about_img" /></div>
-                                    <h2 className="fs-2 fw-bold">Flash cards</h2>
-                                    <p className="mb-0">Flash card style questions.</p>
+                        <Col md="4"  className="about-cards">
+                            <Card className="about-bg">
+                                <CardBody className="about-body p-4 p-lg-5 pt-0 pt-lg-0">
+                                    <div className="about-icons"><img src="/assets/test.png" className="about_img" /></div>
+                                    <CardTitle tag="h2">Flash cards</CardTitle>
+                                    <CardSubtitle tag="h6">Flash card style questions.</CardSubtitle>
                                 </CardBody>
                             </Card>
                         </Col>
-                        <Col md="4" className="mb-5 about-cards">
-                            <Card className="about-bg h-100">
-                                <CardBody className="text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                    <div className="about_icons"><img src="/assets/select.png" class="about_img" /></div>
-                                    <h2 className="fs-2 fw-bold">Test report</h2>
-                                    <p className="mb-0">Comprehensive report of your question/answers for review at the end of your test.</p>
+                        <Col md="4" className="about-cards">
+                            <Card className="about-bg">
+                                <CardBody className="about-body p-4 p-lg-5 pt-0 pt-lg-0">
+                                    <div className="about-icons"><img src="/assets/select.png" className="about_img" /></div>
+                                    <CardTitle tag="h2">Test report</CardTitle>
+                                    <CardSubtitle tag="h6">Comprehensive report of your question/answers for review at the end of your test.</CardSubtitle>
                                 </CardBody>
                             </Card>
                         </Col>
