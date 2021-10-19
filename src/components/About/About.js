7e9943c63a0ba5650e87ccc441e7d408ca18e1e7
Helene <hcblackmer@gmail.com>
2021-10-19 16:57:20 -0700

    Updated About styling.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,8 +10,8 @@ class About extends Component {
                 <Row>
                     <Col md="4" className="about-cards">
                         <Card className="about-bg">
-                            <CardBody className="about-body p-4 p-lg-5 pt-0 pt-lg-0">
-                                <div className="about-icons"><img src="/assets/flash.png" className="about_img" /></div>
+                            <CardBody className="about-body">
+                                <div className="about-icons"><img src="/assets/flash.png" className="about-img" /></div>
                                 <CardTitle tag="h2">A study tool</CardTitle>
                                 <CardSubtitle tag="h6">Customizable and randomized questions.</CardSubtitle>
                             </CardBody>
@@ -19,8 +19,8 @@ class About extends Component {
                     </Col>
                     <Col md="4"  className="about-cards">
                         <Card className="about-bg">
-                            <CardBody className="about-body p-4 p-lg-5 pt-0 pt-lg-0">
-                                <div className="about-icons"><img src="/assets/test.png" className="about_img" /></div>
+                            <CardBody className="about-body">
+                                <div className="about-icons"><img src="/assets/test.png" className="about-img" /></div>
                                 <CardTitle tag="h2">Flash cards</CardTitle>
                                 <CardSubtitle tag="h6">Flash card style questions.</CardSubtitle>
                             </CardBody>
@@ -28,8 +28,8 @@ class About extends Component {
                     </Col>
                     <Col md="4" className="about-cards">
                         <Card className="about-bg">
-                            <CardBody className="about-body p-4 p-lg-5 pt-0 pt-lg-0">
-                                <div className="about-icons"><img src="/assets/select.png" className="about_img" /></div>
+                            <CardBody className="about-body">
+                                <div className="about-icons"><img src="/assets/select.png" className="about-img" /></div>
                                 <CardTitle tag="h2">Test report</CardTitle>
                                 <CardSubtitle tag="h6">Comprehensive report of your question/answers for review at the end of your test.</CardSubtitle>
                             </CardBody>
