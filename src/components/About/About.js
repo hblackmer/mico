3bb57febe353f3fcd78228444133066e579d60c3
Helene <hcblackmer@gmail.com>
2021-10-18 22:45:50 -0700

    Update About component with Reactstrap.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { Card, CardBody } from 'reactstrap';
+import { Container, Row, Col, Card, CardBody } from 'reactstrap';
 import './About.css';
 
 class About extends Component {
     render() {
         return (
             <div className="About">
-                <div className="container px-lg-5">
+                <Container className="about-container px-lg-5">
                     <h2 className="text-white text-center" id="about">What is Mico?</h2>
-                    <div className="row">
-                        <div className="col-md-4 mb-5 about-cards">
+                    <Row>
+                        <Col md="4" className="mb-5 about-cards">
                             <Card className="about-bg h-100">
                                 <CardBody className="text-center p-4 p-lg-5 pt-0 pt-lg-0">
                                     <div className="about_icons"><img src="/assets/flash.png" class="about_img" /></div>
@@ -17,8 +17,8 @@ class About extends Component {
                                     <p className="mb-0">Customizable and randomized questions.</p>
                                 </CardBody>
                             </Card>
-                        </div>
-                        <div className="col-md-4 mb-5 about-cards">
+                        </Col>
+                        <Col md="4"  className="mb-5 about-cards">
                             <Card className="about-bg h-100">
                                 <CardBody className="text-center p-4 p-lg-5 pt-0 pt-lg-0">
                                     <div className="about_icons"><img src="/assets/test.png" class="about_img" /></div>
@@ -26,8 +26,8 @@ class About extends Component {
                                     <p className="mb-0">Flash card style questions.</p>
                                 </CardBody>
                             </Card>
-                        </div>
-                        <div className="col-md-4  mb-5 about-cards">
+                        </Col>
+                        <Col md="4" className="mb-5 about-cards">
                             <Card className="about-bg h-100">
                                 <CardBody className="text-center p-4 p-lg-5 pt-0 pt-lg-0">
                                     <div className="about_icons"><img src="/assets/select.png" class="about_img" /></div>
@@ -35,9 +35,9 @@ class About extends Component {
                                     <p className="mb-0">Comprehensive report of your question/answers for review at the end of your test.</p>
                                 </CardBody>
                             </Card>
-                        </div>
-                    </div>
-                </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
