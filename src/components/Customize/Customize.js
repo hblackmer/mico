ea6504a3c717f29d175cc8f84431fbfefb73b140
Helene <hcblackmer@gmail.com>
2021-10-19 18:59:19 -0700

    Fix to Customization modal.

--- src/components/Customize/Customize.js
+++ src/components/Customize/Customize.js
@@ -55,9 +55,95 @@ class Customize extends Component {
             <div class="modal-dialog modal-lg" role="document">
                 <Modal isOpen={this.props.isModalOpen} toggle={this.props.toggleModal}>
                     <ModalHeader toggle={this.props.toggleModal}>
-                        <h3 className="modal-title">Test Customization Customize</h3>
+                        <h3 className="modal-title">Test Customization</h3>
                         <Button type="button" className="close h3 bg-transparent" data-dismiss="modal">&times;</Button>
                     </ModalHeader>
+                    <ModalBody>
+                        <div class="container-fluid">
+                            <Form id="regForm">
+                                <div class="all-steps" id="all-steps">
+                                    <span class="step"><i class="fas fa-exclamation-triangle"></i></span>
+                                    <span class="step"><i class="far fa-envelope" /></span>
+                                    <span class="step"><i class="fas fa-edit" /></span>
+                                    <span class="step"><i class="far fa-clock" /></span>
+                                </div>
+                                <div class="tab text-center">
+                                    <h3 class="text-danger font-mont">DISCLAIMER</h3>
+                                    <h5 class="text-white my-3">Test customization is in development.<br />
+                                        The following customization options are for demo purposes.</h5>
+                                </div>
+                                <div class="tab">
+                                    <Label class="control-label text-white"></Label>
+                                    <div class="controls">
+                                        <div class="form-check form-switch toggle basic-toggle-button">
+                                            <Input class="form-check-input" type="checkbox" id="emailOption" checked />
+                                            <Label class="form-check-label text-white mb-3 h5" for="emailYes">Email me a
+                                                copy of my test results</Label>
+                                        </div>
+                                    </div>
+                                    <p id="hidden_content" class="mb-5"> 
+                                        <Input placeholder="Email..." oninput="this.className = ''"name="fname" />
+                                    </p>
+                                </div>
+                                <div class="tab">
+                                    <h4 class="text-white text-center font-mont my-3">CATEGORIES</h4>
+                                    <div class="row offset-1 justify-content-center mb-5">
+                                        <div class="form-check form-switch col"> 
+                                            <Input class="form-check-input" type="checkbox" id="flexSwitchCheckHTML" checked />
+                                            <Label class="form-check-label text-white h5"
+                                                for="flexSwitchCheckChecked">HTML</Label>
+                                        </div>
+                                        <div class="form-check form-switch col">
+                                            <Input class="form-check-input" type="checkbox" id="flexSwitchCheckCSS" checked />
+                                            <Label class="form-check-label text-white h5"
+                                                for="flexSwitchCheckChecked">CSS</Label>
+                                        </div>
+                                        <div class="form-check form-switch col">
+                                            <Input class="form-check-input" type="checkbox" id="flexSwitchCheckJavaScript"
+                                                checked />
+                                            <Label class="form-check-label text-white h5"
+                                                for="flexSwitchCheckChecked">JavaScript</Label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="tab">
+                                    <h4 class="text-white text-center font-mont my-3">LENGTH</h4>
+                                    <div class="toggle_radio mb-5">
+                                        <Input type="radio" class="toggle_option" id="first_toggle" name="toggle_option" />
+                                        <Input type="radio" checked class="toggle_option" id="second_toggle" name="toggle_option" />
+                                        <Input type="radio" class="toggle_option" id="third_toggle" name="toggle_option" />
+                                        <Label for="first_toggle">
+                                            <p class="h5">Short</p>
+                                        </Label>
+                                        <Label for="second_toggle">
+                                            <p class="h5">Medium</p>
+                                        </Label>
+                                        <Label for="third_toggle">
+                                            <p class="h5">Long</p>
+                                        </Label>
+                                        <div class="toggle_option_slider">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="tab text-center">
+                                    <h3 class="text-danger font-mont">DISCLAIMER</h3>
+                                    <h5 class="text-white my-3">Website is in development. <br />
+                                        The next pages are for demo purposes.</h5>
+                                    <a href="mico.html" class="btn btn-danger text-white" role="button">Demo</a>
+                                </div>
+                                <div style={{overflow: "auto"}} id="nextprevious">
+                                    <div style={{float: "right"}}>
+                                        <Button type="button" id="prevBtn" onclick="nextPrev(-1)">
+                                            <i class="fa fa-angle-double-left" />
+                                        </Button>
+                                        <Button type="button" id="nextBtn" onclick="nextPrev(1)">
+                                            <i class="fa fa-angle-double-right" />
+                                        </Button>
+                                    </div>
+                                </div>
+                            </Form>
+                        </div>
+                    </ModalBody>
                 </Modal>
             </div>
         )
