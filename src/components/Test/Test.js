9b4e6887c31f37ae63864b0feb1a908bb7aa98ac
Helene <hcblackmer@gmail.com>
2021-10-21 10:19:58 -0700

    Updated styling in Test.

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -29,35 +29,35 @@ class Test extends Component {
         const { showAnswer } = this.state;
 
         return (
-            <div className="Test">
+            <Container className="Test">
                 <div className="dark-overlay"></div>
-                <Container className="d-flex justify-content-center align-items-center">
-                    <Row>
-                        <Col sm={{ size: 10, offset: 1 }}>
-                            <Row className="category">
-                                <h1 className="mb-0 text-green text-center"><i className="fab fa-css3-alt" /> CSS </h1>
-                            </Row>
-                            <Row>
-                                <Col className="progress text-center mb-3">
-                                    <div className="progress-value">Question 3 of 5</div>
-                                </Col>
-                            </Row>
-                            <Timer active={this.state.timerActive} duration={null} className="text-green">
-                                <i className="fas fa-stopwatch" />
-                                <span>  </span>
-                                <Timecode />
-                            </Timer>
-                            <div className="question">
-                                <h4 className="row text-white">What are CSS selectors?</h4>
-                            </div>
-                            { showAnswer ? 
-                            <Answer /> : 
-                            <Button variant="primary" className="btn btn-primary" id="start_btn" onClick={() => this.hideComponent()}>START</Button>
-                             }
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+                <Row>
+                    <Col sm={{ size: 10, offset: 1 }}>
+                        <Row className="category">
+                            <Col className="test-category">
+                                <i className="fab fa-css3-alt" /> CSS
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className="progress">
+                                <div className="progress-value">Question 3 of 5</div>
+                            </Col>
+                        </Row>
+                        <Timer active={this.state.timerActive} duration={null} className="text-green">
+                            <i className="fas fa-stopwatch" />
+                            <span>  </span>
+                            <Timecode />
+                        </Timer>
+                        <div className="question">
+                            <h4 className="row text-white">What are CSS selectors?</h4>
+                        </div>
+                        { showAnswer ? 
+                        <Answer /> : 
+                        <Button variant="primary" className="btn btn-primary" id="start_btn" onClick={() => this.hideComponent()}>START</Button>
+                            }
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
