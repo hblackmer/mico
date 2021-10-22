bc19b0286b2c46dba32bfd18bc731f2c2bbf8f40
Helene <hcblackmer@gmail.com>
2021-10-22 15:20:40 -0700

    Updated Footer class component to functional component.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Footer.css';
+
+// Logo Icon
 import logoImg from '../../img/logo_circle.png';
 
-class Footer extends Component {
-    render() {
-        return (
-            <footer class="Footer">
-                <div class="container">
-                    <div class="row">
-                        <div class="col d-inline-flex justify-content-center mt-3">
-                            <a href="https://heleneblackmer.com" target="_blank"><img class="footer_icons hb_logo"
-                                src={logoImg} /></a>
-                            <a href="https://github.com/hblackmer" target="_blank"><i class="fab fa-github media_icons"></i></a>
-                            <a href="https://www.linkedin.com/in/hblackmer" target="_blank"><i
-                                class="fab fa-linkedin media_icons"></i></a>
-                        </div>
+export default function Footer() {
+    return (
+        <footer class="Footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col d-inline-flex justify-content-center mt-3">
+                        <a href="https://heleneblackmer.com" target="_blank"><img class="footer_icons hb_logo"
+                            src={logoImg} /></a>
+                        <a href="https://github.com/hblackmer" target="_blank"><i class="fab fa-github media_icons"></i></a>
+                        <a href="https://www.linkedin.com/in/hblackmer" target="_blank"><i
+                            class="fab fa-linkedin media_icons"></i></a>
                     </div>
                 </div>
-            </footer>
-        );
-    }
+            </div>
+        </footer>
+    );
 }
-
-export default Footer;
