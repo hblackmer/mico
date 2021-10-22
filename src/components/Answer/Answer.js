4a8e4e871957a5d84d9c73d5c6bc87a035ac1286
Helene <hcblackmer@gmail.com>
2021-10-22 16:44:20 -0700

    Added React Redux form.

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,35 +1,48 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import {
     Button,
-    Container, Row, Col,
-    Form, FormGroup, Input, Label,
+    Row, Col,
+    Label,
 } from 'reactstrap';
+import { Control, LocalForm } from 'react-redux-form';
 import '../Test/Test.css';
 
 class Answer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            answer: ''
+        }
+    };
+
     render() {
         return (
-            <Fragment>
-                <Form id="answer-textarea">
-                    <Row className="form-group mt-3">
-                        <Col className="answer-text">
-                            <Label for="feedback" className="col-form-label fs-3">Your Answer:</Label>
-                                <textarea className="form-control" id="answer" name="answer" rows="6"></textarea>
-                        </Col>
-                    </Row>
-                    <Row className="form-group mt-3">
-                        <Col>
-                            <Button type="button" color="primary" className="answer-submit" onClick={this.props.submit}>Submit</Button>
-                            <Button type="button" className="answer-arrow bg-transparent text-success" onClick={this.props.next}>
-                                <i className="far fa-caret-square-right" />
-                            </Button>
-                            <Button type="button" className="answer-arrow bg-transparent text-success" onClick={this.props.prev}>
-                                <i className="far fa-caret-square-left" />
-                            </Button>
-                        </Col>
-                    </Row>
-                </Form>
-            </Fragment>
+            <LocalForm onSubmit={values => this.props.submit(values)} id="answer-textarea">
+                <Row className="form-group mt-3">
+                    <Col className="answer-text">
+                        <Label htmlFor="answer">Your Answer:</Label>
+                        <Control.textarea
+                            model=".answer"
+                            id="answer"
+                            name="answer"
+                            rows="8"
+                            className="form-control"
+                        />
+                    </Col>
+                </Row>
+                <Row className="form-group mt-3">
+                    <Col>
+                        <Button type="button" color="primary" className="answer-submit" onClick={this.props.submit}>Submit</Button>
+                        <Button type="button" className="answer-arrow bg-transparent text-success" onClick={this.props.next}>
+                            <i className="far fa-caret-square-right" />
+                        </Button>
+                        <Button type="button" className="answer-arrow bg-transparent text-success" onClick={this.props.prev}>
+                            <i className="far fa-caret-square-left" />
+                        </Button>
+                    </Col>
+                </Row>
+            </LocalForm>
         );
     }
 }
