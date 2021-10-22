bd2de2ade4186e5e2c97a619968c77da7d8156b6
Helene <hcblackmer@gmail.com>
2021-10-22 11:10:11 -0700

    Updated images.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ class Footer extends Component {
                     <div class="row">
                         <div class="col d-inline-flex justify-content-center mt-3">
                             <a href="https://heleneblackmer.com" target="_blank"><img class="footer_icons hb_logo"
-                                src="/assets/logo_circle.png" /></a>
+                                src="../../img/logo_circle.png" /></a>
                             <a href="https://github.com/hblackmer" target="_blank"><i class="fab fa-github media_icons"></i></a>
                             <a href="https://www.linkedin.com/in/hblackmer" target="_blank"><i
                                 class="fab fa-linkedin media_icons"></i></a>
