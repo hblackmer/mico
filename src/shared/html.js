08e3a464242a02fcd88ba8b120087e9da8bb958c
Helene <hcblackmer@gmail.com>
2021-10-22 21:04:00 -0700

    Added CSS and HTML questions.

---
+++ src/shared/html.js
@@ -0,0 +1,14 @@
+export const CSS = [
+    {
+        id: 0,
+        question: "What is HTML?",
+        answer: "HTML is the standard markup language for creating Web pages.",
+        source: "https://www.w3schools.com/html/html_intro.asp"
+    },
+    {
+        id: 1,
+        question: "How can we comment in HTML?",
+        answer: "The commented outlines will not be shown in the browser. To comment a line, the line should start by this <!– and end by this –>. Comments can be of one line or of multiple lines.",
+        source: "https://www.w3schools.com/html/html_comments.asp"
+    }
+];
