6f90f220d076bc1b52125ea8a020bc9cf8a6e9e2
Helene <hcblackmer@gmail.com>
2021-10-24 21:40:10 -0700

    Update Answer component CSS and minor cleanup.

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import {
     Button,
     Row, Col,
@@ -19,37 +19,55 @@ function Answer ({id, answer, prev, next, submit, lastQuestion, addAnswer}) {
         <Form model="feedbackForm" onSubmit={values => submit(values)} id="answer-textarea">
             <Row className="form-group mt-3">
                 {!lastQuestion ?
-                <Col className="answer-text">
-                    <Label htmlFor="answer">Your Answer:</Label>
-                    <Control.textarea
-                        model=".answer"
-                        id="answer"
-                        name="answer"
-                        rows="6"
-                        className="form-control"
-                        onChange={handleInputChange}
-                        value={value}
-                    />
-                </Col>:''};
+                    <Col className="answer-text">
+                        <Label htmlFor="answer">Your Answer:</Label>
+                        <Control.textarea
+                            model=".answer"
+                            id="answer"
+                            name="answer"
+                            rows="6"
+                            className="form-control"
+                            onChange={handleInputChange}
+                            value={value}
+                        />
+                    </Col>:''
+                };
             </Row>
             <Row className="form-group mt-3">
                 {lastQuestion ?
-                <Fragment>
+                    <Fragment>
+                        <Col xs={12} className="justify-content-center d-flex">
+                            <Button 
+                                type="button"
+                                outline
+                                id="answer-back"
+                                onClick={prev}
+                            >Go Back</Button>
+                        </Col>
+                        <Col xs={12} className="justify-content-center d-flex">
+                            <Button 
+                                type="button"
+                                color="primary"
+                                id="answer-submit"
+                                onClick={submit}
+                            >Submit Test</Button>
+                        </Col>
+                    </Fragment> :
                     <Col xs={12} className="justify-content-center d-flex">
-                        <Button type="button" outline id="answer-back" onClick={prev}>Go Back</Button>
+                        <Button 
+                            type="button" 
+                            className="answer-arrow bg-transparent text-success"
+                            onClick={prev}
+                            ><i className="far fa-caret-square-left" />
+                        </Button>
+                        <Button
+                            type="submit"
+                            className="answer-arrow bg-transparent text-success"
+                            onClick={next}
+                            ><i className="far fa-caret-square-right" />
+                        </Button>
                     </Col>
-                    <Col xs={12} className="justify-content-center d-flex">
-                        <Button type="button" color="primary" className="answer-submit" onClick={submit}>Submit Test</Button>
-                    </Col>
-                </Fragment> :
-                <Col xs={12} className="justify-content-center d-flex">
-                    <Button type="button" className="answer-arrow bg-transparent text-success" onClick={prev}>
-                        <i className="far fa-caret-square-left" />
-                    </Button>
-                    <Button type="submit" className="answer-arrow bg-transparent text-success" onClick={next}>
-                        <i className="far fa-caret-square-right" />
-                    </Button>
-                </Col>}
+                }
             </Row>
         </Form>
     );
