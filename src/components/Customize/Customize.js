a7d4feb42706991aab751840a09416a1989426bb
Helene <hcblackmer@gmail.com>
2021-10-24 22:51:23 -0700

    Minor cleanup in Customize component.

--- src/components/Customize/Customize.js
+++ src/components/Customize/Customize.js
@@ -1,51 +1,72 @@
 import React, { Component } from 'react';
 import {
     Modal, ModalHeader, ModalBody,
-    Button, Tooltip,
-    Form, FormGroup, Input, Label,
-    Row, Col
+    Button,
+    Form, Input, Label,
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import Categories from '../Categories/Categories';
 import './Customize.css';
 
 
-class Customize extends Component {
+class Customize extends Component {    
     render() {
+        const { isModalOpen, toggleModal } = this.props;
+
         return (
-            <Modal isOpen={this.props.isModalOpen} toggleModal={this.props.toggleModal}>
-                <ModalHeader toggle={this.props.toggleModal}> 
+            <Modal isOpen={isModalOpen} toggleModal={toggleModal}>
+                <ModalHeader toggle={toggleModal}> 
                     <h3>Test Customization</h3>
                 </ModalHeader>
                 <ModalBody>
                     <Form id="regForm">
-                        <div className="tab">
+                        <div className="customize-section">
                             <h4 className="categories-header">CATEGORIES</h4>
                             <lead className="categories-subtitle">Drag and drop item(s) to be tested on:</lead>
                             <Categories />
                         </div>
-                        <div className="tab">
+                        <div className="customize-section">
                             <h4 className="length-header">LENGTH</h4>
                             <div className="toggle-radio mb-5">
-                                <Input type="radio" className="toggle-option" id="first-toggle" name="toggle-option" />
-                                <Input type="radio" checked className="toggle-option" id="second-toggle" name="toggle-option" />
-                                <Input type="radio" className="toggle-option" id="third-toggle" name="toggle-option" />
-                                <Label for="first-toggle">
+                                <Label htmlFor="first-toggle">
                                     <h5>Short</h5>
                                 </Label>
-                                <Label for="second-toggle">
+                                <Input
+                                    type="radio"
+                                    className="toggle-option"
+                                    id="first-toggle"
+                                    name="toggle-option"
+                                />
+                                <Label htmlFor="second-toggle">
                                     <h5>Medium</h5>
                                 </Label>
-                                <Label for="third-toggle">
+                                <Input
+                                    type="radio"
+                                    checked
+                                    className="toggle-option"
+                                    id="second-toggle"
+                                    name="toggle-option"
+                                />
+                                <Label htmlFor="third-toggle">
                                     <h5>Long</h5>
                                 </Label>
+                                <Input
+                                    type="radio"
+                                    className="toggle-option"
+                                    id="third-toggle"
+                                    name="toggle-option"
+                                />
                                 <div className="toggle-option-slider">
                                 </div>
                             </div>
                         </div>
                         <div className="tab text-center">
                             <Link to="/mico/test">
-                                <Button color="primary" id="customize-button" onClick={this.props.toggleModal}>Start</Button>
+                                <Button 
+                                    color="primary"
+                                    id="customize-button"
+                                    onClick={toggleModal}
+                                >Start</Button>
                             </Link>
                         </div>
                     </Form>
