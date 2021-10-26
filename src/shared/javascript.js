2374563b0382bf038d05a02cc93d2d74594db03a
Helene <hcblackmer@gmail.com>
2021-10-26 12:48:37 -0700

    Added more questions for JavaScript category.

--- src/shared/javascript.js
+++ src/shared/javascript.js
@@ -30,5 +30,69 @@ export const JAVASCRIPT = [
         micoAnswer: "Prototypes are the mechanism by which JavaScript objects inherit features from one another",
         source: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes",
         category: "javascript"
+    },
+    {
+        id: 4,
+        question: "What are arrow functions?",
+        answer: "",
+        micoAnswer: "Arrow functions were introduced in the ES6 version of javascript. They provide us with a new and shorter syntax for declaring functions. Arrow functions can only be used as a function expression.",
+        source: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions",
+        category: "javascript"
+    },
+    {
+        id: 5,
+        question: "What are the differences between declaring variables using var, let and const?",
+        answer: "",
+        micoAnswer: "The scope of a variable defined with the keyword 'var' is limited to the function within which it is defined. If it is defined outside any function, the scope of the variable is global. The scope of a variable defined with the keyword 'let' or 'const' is limited to the block defined by curly braces i.e. {}.",
+        source: "https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/",
+        category: "javascript"
+    },
+    {
+        id: 6,
+        question: "What is the rest parameter and spread operator?",
+        answer: "",
+        micoAnswer: "Rest parameter collects all remaining elements into an array. Spread operator allows iterables (arrays/objects/strings) to be expanded into single arguments/elements.",
+        source: "https://scotch.io/bar-talk/javascripts-three-dots-spread-vs-rest-operators543",
+        category: "javascript"
+    },
+    {
+        id: 7,
+        question: "What is the use of promises in JavaScript?",
+        answer: "",
+        micoAnswer: "Promises are used to handle asynchronous operations in javascript.",
+        source: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise",
+        category: "javascript"
+    },
+    {
+        id: 8,
+        question: "What are classes in JavaScript?",
+        answer: "",
+        micoAnswer: "Introduced in the ES6 version, classes are syntactic sugar for constructor functions. They provide a new way of declaring constructor functions in javascript.",
+        source: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes",
+        category: "javascript"
+    },
+    {
+        id: 9,
+        question: "What is Object Destructuring?",
+        answer: "",
+        micoAnswer: "Object destructuring is a new way to extract elements from an object or an array.",
+        source: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment",
+        category: "javascript"
+    },
+    {
+        id: 10,
+        question: "What is the difference between '==' and '===' operators?",
+        answer: "",
+        micoAnswer: "Both are comparison operators. The difference between both the operators is that, '==' is used to compare values whereas, '===' is used to compare both value and types.",
+        source: "https://www.geeksforgeeks.org/difference-between-and-operator-in-javascript/",
+        category: "javascript"
+    },
+    {
+        id: 11,
+        question: "Explain passed by value and passed by reference.",
+        answer: "",
+        micoAnswer: "In JavaScript, primitive data types are passed by value and non-primitive data types are passed by reference.",
+        source: "https://pediaa.com/what-is-the-difference-between-pass-by-value-and-pass-by-reference/",
+        category: "javascript"
     }
 ];
