8326da7a75696677b83d3766368fb64d0cd67ba3
Helene <hcblackmer@gmail.com>
2021-10-26 12:36:33 -0700

    Added more questions for HTML and CSS.

--- src/shared/htmlcss.js
+++ src/shared/htmlcss.js
@@ -17,6 +17,38 @@ export const HTMLCSS = [
     },
     {
         id: 2,
+        question: "What are Attributes and how do you use them?",
+        answer: "",
+        micoAnswer: "Each tag has additional attributes that change the way the tag behaves or is displayed. Attributes are specified directly after the name of the tag, inside the two angled brackets.",
+        source: "https://www.w3schools.com/html/html_attributes.asp",
+        category: "html"
+    },
+    {
+        id: 3,
+        question: "What is the difference between a block-level element and an inline element?",
+        answer: "",
+        micoAnswer: "A block-level element always starts on a new line while an inline element does not start on a new line. A block-level element always takes up the full width available (stretches out to the left and right as far as it can) while an inline element only takes up as much width as necessary.",
+        source: "https://www.w3schools.com/html/html_blocks.asp",
+        category: "html"
+    },
+    {
+        id: 4,
+        question: "What is semantic HTML?",
+        answer: "",
+        micoAnswer: "A semantic element clearly describes its meaning to both the browser and the developer.",
+        source: "https://www.w3schools.com/html/html5_semantic_elements.asp",
+        category: "html"
+    },
+    {
+        id: 5,
+        question: "What is the purpose of using alternative texts in images?",
+        answer: "",
+        micoAnswer: "The purpose of using alternative texts is to define what the image is about.",
+        source: "https://moz.com/learn/seo/alt-text",
+        category: "html"
+    },
+    {
+        id: 6,
         question: "What is CSS?",
         answer: "",
         micoAnswer: "CSS is a language for specifying how documents are presented to users.",
@@ -24,7 +56,7 @@ export const HTMLCSS = [
         category: "css"
     },
     {
-        id: 3,
+        id: 7,
         question: "What is the difference between class selectors and ID selectors?",
         answer: "",
         micoAnswer: "In the CSS, a class selector is a name preceded by a full stop ('.') and an ID selector is a name preceded by a hash character ('#'). The difference between an ID and a class is that an ID can be used to identify one element, whereas a class can be used to identify more than one.",
@@ -32,7 +64,7 @@ export const HTMLCSS = [
         category: "css"
     },
     {
-        id: 4,
+        id: 8,
         question: "What is the float property of CSS?",
         answer: "",
         micoAnswer: "The float property specifies whether an element should float to the left, right, or not at all.",
@@ -40,11 +72,27 @@ export const HTMLCSS = [
         category: "css"
     },
     {
-        id: 5,
+        id: 9,
         question: "What is the purpose of the z-index and how is it used?",
         answer: "",
         micoAnswer: "The z-index helps to specify the stack order of positioned elements that may overlap one another. The z-index default value is zero and can take on either a positive or negative number.",
         source: "https://www.w3schools.com/cssref/pr_pos_z-index.asp",
         category: "css"
-    }
+    },
+    {
+        id: 10,
+        question: "What is a CSS framework?",
+        answer: "",
+        micoAnswer: "CSS frameworks are tools used by UI developers to make their job easier. Rather than reinventing the wheel each time a new project comes up; frameworks give developers the tools to quickly spin-up user interfaces that can be tweaked and iterated on throughout a project instead of spending time starting from a blank document.",
+        source: "https://medium.com/html-all-the-things/what-is-a-css-framework-f758ef0b1a11",
+        category: "css"
+    },
+    {
+        id: 11,
+        question: "Explain the CSS Box Model and its different elements.",
+        answer: "",
+        micoAnswer: "The CSS box model describes the rectangular boxes that are generated for elements in the document tree and laid out according to the visual formatting model. Each box has a content area (e.g. text, an image, etc.) and an optional surrounding padding, border, and margin areas.",
+        source: "https://www.w3schools.com/css/css_boxmodel.asp",
+        category: "css"
+    },
 ];
