9dce1fa2dea686b5efcbe129a7a3bbcf80d65e48
Helene <hcblackmer@gmail.com>
2021-11-12 11:18:24 -0800

    Updated footer icons with tooltip description.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,16 +12,25 @@ export default function Footer() {
         <Fragment>
             <Container>
                 <Row>
-                    <Col className="d-inline-flex justify-content-center mt-3">
-                        <a href="https://heleneblackmer.com" rel="noopener noreferrer" target="_blank">
-                            <img className="footer_icons hb_logo" alt="Main website logo" src={logoImg} />
-                        </a>
-                        <a href="https://github.com/hblackmer" rel="noopener noreferrer" alt="GitHub logo" target="_blank">
-                            <i className="fab fa-github media_icons" />
-                        </a>
-                        <a href="https://www.linkedin.com/in/hblackmer" rel="noopener noreferrer" alt="LinkedIn logo" target="_blank">
-                            <i className="fab fa-linkedin media_icons" />
-                        </a>
+                    <Col className="d-inline-flex justify-content-center mt-1 wrapper">
+                        <div class="icon logo">
+                        <div class="tooltip">Portfolio</div>
+                            <a href="https://heleneblackmer.com" rel="noopener noreferrer" target="_blank">
+                                <img className="footer_icons hb_logo" alt="Main website logo" src={logoImg} />
+                            </a>
+                        </div>
+                        <div class="icon github">
+                            <div class="tooltip">GitHub</div>
+                            <a href="https://github.com/hblackmer" rel="noopener noreferrer" alt="GitHub logo" target="_blank">
+                                <i className="fab fa-github media_icons" />
+                            </a>
+                        </div>
+                        <div class="icon linkedin">
+                            <div class="tooltip">LinkedIn</div>
+                            <a href="https://www.linkedin.com/in/hblackmer" rel="noopener noreferrer" alt="LinkedIn logo" target="_blank">
+                                <i className="fab fa-linkedin media_icons" />
+                            </a>
+                        </div>
                     </Col>
                 </Row>
             </Container>
